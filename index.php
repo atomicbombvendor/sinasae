dc0f735a3565003a7d9113dbab8fa9c587e171f9
Eddie.Li <Eddie.Li@morningstar.com>
2018-05-05 17:24:37 +0800

    traceHttp

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 
 header('Content-type:text');
 define("TOKEN", "weixin");
+traceHttp();
 $wechatObj = new wechatCallbackapiTest();
 echo "hello";
 if(isset($_GET['echostr'])){
@@ -73,6 +74,25 @@ class wechatCallbackapiTest
         }
     }
 }
+
+function traceHttp(){
+    $content = date('Y-m-d H:i:s')."\nREMOTE_ADDR:".$_SERVER["REMOTE_ADDR"]."\nQUERY_STRING:".$_SERVER["QUERY_STRING"]."\n\n";
+
+    if(isset($_SERVER["HTTP_APPNAME"])){
+        //SAE
+        sae_set_display_errors(false);
+        sae_debug(trim($content));
+        sae_set_display_errors(true);
+    }else{
+        $max_size = 100000;
+        $log_filename = "log.xml";
+        if(file_exists($log_filename) and abs(filesize($log_filename)) > $max_size){
+            unlink($log_filename);
+        }
+        file_put_contents($log_filename, $content, FILE_APPEND);
+    }
+}
+
 
 
 ?>
