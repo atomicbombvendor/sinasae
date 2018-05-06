af318469eda8162a635824ac4dada019a5a22db5
Eddie.Li <Eddie.Li@morningstar.com>
2018-05-06 14:58:39 +0800

    test 2

--- index.php
+++ index.php
@@ -63,9 +63,7 @@ class wechatCallbackapiTest
             $toUserName = $postObj->ToUserName;
             $keyword = trim($postObj->Content);
             $time = time();
-            $textTpl = "<xml><ToUserName>< ![CDATA[%s] ]></ToUserName> <FromUserName>< ![CDATA[%s] ]></FromUserName> "
-                ."<CreateTime>%s</CreateTime> <MsgType>< ![CDATA[%s] ]></MsgType> "
-                ."<Content>< ![CDATA[%s] ]></Content></xml>";
+            $textTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName> <FromUserName><![CDATA[%s]]></FromUserName> <CreateTime>%s</CreateTime> <MsgType><![CDATA[%s]]></MsgType> <Content><![CDATA[%s]]></Content></xml>";
 
             if($keyword == "?" || $keyword == "？"){
                 $msgType = "text";
